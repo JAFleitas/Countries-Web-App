1b7b40ab9b1c683cd510b4afea805daf57373e0f
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-04-22 16:16:17 -0300

    modificando estilos responsive

--- client/src/Components/Countries/Countries.jsx
+++ client/src/Components/Countries/Countries.jsx
@@ -1,111 +1,56 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Country from "../Country/Country";
-import { useSelector, useDispatch } from 'react-redux'
-import { get_countries} from "../../actions";
-import style from './Countries.module.css'
+import { useSelector, useDispatch } from "react-redux";
+import { get_countries } from "../../actions";
+import style from "./Countries.module.css";
 import ContainerFilters from "../ContainerFilter/ContainerFilters";
 
 import Loading from "../loading/Loading";
 import Paginado from "../Paginado/Paginado";
 
+const Countries = () => {
+  const countries = useSelector((state) => state.countries);
 
-const Countries = () =>{
+  const dispatch = useDispatch();
+  // uso el filtro del estado global y un useEfect para poder volver a renderizar los paises, ya el que el sort solo ordena el array
+  const filtro = useSelector((state) => state.filtro);
+  useState(() => console.log(filtro), [filtro]);
 
-    const countries = useSelector(state => state.countries)
-    
-    const dispatch = useDispatch()
-    // uso el filtro del estado global y un useEfect para poder volver a renderizar los paises, ya el que el sort solo ordena el array 
-    const filtro = useSelector(state=> state.filtro)
-    useState(()=> console.log(filtro),[filtro])
-    
-    
-    useEffect(() => {
-        dispatch(get_countries())
-     
-    },[])
-    const loading = useSelector(state => state.loading)
-    
-   //PAGINADO
-   const inicio = useSelector(state=> state.inicio)
-   const fin = useSelector(state=> state.fin)
+  useEffect(() => {
+    dispatch(get_countries());
+  }, [dispatch]);
+  const loading = useSelector((state) => state.loading);
 
-   /* 
-    const paisesPorPag = 10
-    const [ numDePag, setNumDePag ] = useState(1)
-    const inicio = ( numDePag * paisesPorPag ) - paisesPorPag
-    const fin = ( numDePag * paisesPorPag )
-    const cantDePag = Math.ceil(countries.length / paisesPorPag)
+  //PAGINADO
+  const inicio = useSelector((state) => state.inicio);
+  const fin = useSelector((state) => state.fin);
 
-    const previousPage = () =>{
-       if( numDePag === 1) return
-       setNumDePag(numDePag - 1)
-    }
-    const nextPage = () => {
-        if (numDePag===cantDePag) return ;
-        setNumDePag(numDePag + 1)
-    }
-
-    const buttonPage = num =>{
-        setNumDePag(num)
-    }
-
-    const createArr = num =>{
-        const array = []
-        for(let i= 1; i<=num ;i++){
-            array.push(i)
-
-        }
-        return array
-    }
-
-    const paginado = createArr(cantDePag)
-
-    console.log(paginado)
- */
-    const display = countries.slice( inicio, fin ).map(c => <Country
-        name ={c.name}
-        flag={c.flag[1]} 
-        continent= {c.continent} 
+  const display = countries
+    .slice(inicio, fin)
+    .map((c) => (
+      <Country
+        name={c.name}
+        flag={c.flag[1]}
+        continent={c.continent}
         key={c.id}
         id={c.id}
-    />)
-    
-    
-    return (
-        <div className={style.container}>
-            <div className={style.subContainer}>
-                <div className={style.filtros}>
+      />
+    ));
 
-                     <ContainerFilters/>
-
-                </div>
-
-                <div className={style.containerCountries}>
-                    {
-                    loading? <Loading/> :  display 
-                    }
-                </div>
-            </div>
-            
-                
-{/*             <div className={style.boton}>
-
-                    <button onClick={previousPage}>Anterior</button>
-
-                    {paginado.map(e =>{
-
-                        return <button className={numDePag === e ? style.activo : null} key={e} onClick= {()=> buttonPage(e)}>{e}</button>
-
-                    })}
-
-                    <button onClick={nextPage}>Proximo</button>
-            </div> */}
-            
-           { <Paginado /> }
-
-                
-        
+  return (
+    <div className={style.container}>
+      <div className={style.subContainer}>
+        <div className={style.filtros}>
+          <ContainerFilters />
         </div>
-    )
-}
+
+        <div className={style.containerCountries}>
+          {loading ? <Loading /> : display}
+        </div>
+      </div>
+
+      {<Paginado />}
+    </div>
+  );
+};
 export default Countries;
--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,44 +1,40 @@
-import React , {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { get_countries } from "../../actions";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import style from './NavBar.module.css'
+import style from "./NavBar.module.css";
 
 const NavBar = () => {
-    const [namePais, setNamePais ] = useState({name : ''})
-    const dispatch = useDispatch()
+  const [namePais, setNamePais] = useState({ name: "" });
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(get_countries(namePais.name))
-     
-    },[namePais])
+  useEffect(() => {
+    dispatch(get_countries(namePais.name));
+  }, [namePais, dispatch]);
 
-    const handleChange = e => {
-        setNamePais ({name: e.target.value})
-    }
+  const handleChange = (e) => {
+    setNamePais({ name: e.target.value });
+  };
 
+  return (
+    <div className={style.container}>
+      <header>Henry's Countries App</header>
+      <div className={style.input}>
+        <input
+          value={namePais.name}
+          onChange={(e) => handleChange(e)}
+          placeholder="Paises"
+        />
+      </div>
 
-
-    
-    return (
-        <div className={style.container}>
-            <div className={style.input}>
-                <header>Henry's Countries App</header>
-                <input value = {namePais.name}
-                onChange = {e => handleChange(e) 
-                } placeholder='Paises'
-                />
-            </div>
-
-            <div className={style.actividad}>
-                <Link to='/create_activity'>
-                    <button>Crear Actividad</button>
-                </Link>
-            </div>
-
-        </div>
-    )
-}
+      <div className={style.actividad}>
+        <Link to="/create_activity">
+          <button>Crear Actividad</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
